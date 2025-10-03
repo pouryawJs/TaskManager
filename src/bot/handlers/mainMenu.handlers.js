5081916a5d6cdbeae2afa254d433b5b804864857
pouryawJS <pourya.bu@gmail.com>
2025-10-03 22:10:51 +0330

    Feat: menu command

--- src/bot/handlers/mainMenu.handlers.js
+++ src/bot/handlers/mainMenu.handlers.js
@@ -2,6 +2,17 @@ const safeAction = require("../../utils/safeAction");
 const mainMenuUI = require("../../services/ui/mainMenu.ui.service");
 
 module.exports = (bot) => {
+	bot.command("menu", async (ctx) => {
+		if (ctx.scene && ctx.scene.current) {
+			try {
+				await ctx.scene.leave();
+			} catch (e) {
+				console.error("leave scene error:", e);
+			}
+		}
+		mainMenuUI.showMainMenu(ctx, true);
+	});
+
 	safeAction(bot, "MAIN_MENU", (ctx) => mainMenuUI.showMainMenu(ctx));
 
 	safeAction(bot, "GUIDE", (ctx) => mainMenuUI.showGuide(ctx));
--- src/services/ui/mainMenu.ui.service.js
+++ src/services/ui/mainMenu.ui.service.js
@@ -1,11 +1,18 @@
 const mainMenuKeyboard = require("../../bot/keyboards/mainMenu.keyboard");
 const mainMenuMessage = require("../../bot/messages/mainMenu.message");
 
-exports.showMainMenu = (ctx) => {
-	ctx.editMessageText(mainMenuMessage.mainMenu(), {
-		parse_mode: "HTML",
-		...mainMenuKeyboard.mainMenu(),
-	});
+exports.showMainMenu = (ctx, isCommand = false) => {
+	if (!isCommand) {
+		ctx.editMessageText(mainMenuMessage.mainMenu(), {
+			parse_mode: "HTML",
+			...mainMenuKeyboard.mainMenu(),
+		});
+	} else {
+		ctx.reply(mainMenuMessage.mainMenu(), {
+			parse_mode: "HTML",
+			...mainMenuKeyboard.mainMenu(),
+		});
+	}
 };
 
 exports.showGuide = (ctx) => {
